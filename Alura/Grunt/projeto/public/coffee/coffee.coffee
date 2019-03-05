1ea4f87e46784ca173fa8dff7e7f8f79f326d306
xb195192 <dbcorreia17@gmail.com>
2019-03-05 00:43:23 -0300

    approve delete files

--- Alura/Grunt/projeto/public/coffee/coffee.coffee
+++
@@ -1,1 +0,0 @@
-nome='Daniel Bertolini Correia';
--- Alura/Javascript/Avancado/aluraframe/client/js/app-es6/boot.js
+++
@@ -1,5 +0,0 @@
-import {getInstance} from './controllers/NegociacaoController';
-let negociacaoController = getInstance();
-
-document.querySelector('.form').onsubmit = negociacaoController.add.bind(negociacaoController);
-document.querySelector('#apaga').onclick = negociacaoController.esvazia.bind(negociacaoController);
